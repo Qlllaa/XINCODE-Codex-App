a3067a9a658fcb6a5e4ed064f791a6b3e2e32b78
Qlllaa <178182738@qq.com>
2026-07-25 15:52:06 +0800

    add core/build.gradle.kts

---
+++ core/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.codex.core"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 28
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":data"))
+    implementation(project(":provider"))
+    implementation(project(":security"))
+    
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.json:json:20231013")
+}
